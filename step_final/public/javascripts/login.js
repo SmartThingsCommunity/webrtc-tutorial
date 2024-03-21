c523ac72824c12af4e11221a5f60b9fdee650dba
Brian Shoultz <brian.shoultz@smartthings.com>
2024-03-21 13:22:22 -0500

    removing redundant step_final files

--- step_final/public/javascripts/login.js
+++
@@ -1,6 +0,0 @@
-/* global $ */
-$( document ).ready(function() {
-	$('input[name=\'login\']').click(function() {
-		$(this).addClass('processing')
-	})
-})
--- step_final/src/lib/handlers/integration-deleted.js
+++
@@ -1,5 +0,0 @@
-const { deleteConnection } = require('../db')
-
-module.exports = async (accessToken) => {
-	await deleteConnection(accessToken)
-}
